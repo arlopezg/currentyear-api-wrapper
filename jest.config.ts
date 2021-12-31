309e6b90899eb4491c0913768d81f57294cb6ed1
Alejandro López <alejandrolopez.gd@gmail.com>
2021-12-31 17:35:14 -0300

    chore: generate coverage report and badges

--- jest.config.ts
+++ jest.config.ts
@@ -4,4 +4,6 @@ export default {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFiles: ["./tests/setupJest.ts"],
+  coverageReporters: ["json-summary", "text", "lcov"],
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
 } as Config.InitialOptions;
