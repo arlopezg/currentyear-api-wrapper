04518a84573c9c455a5fa52cc0a3aa86ea265111
Alejandro López <alejandrolopez.gd@gmail.com>
2022-01-12 21:58:16 -0300

    chore: remove self dep

--- src/utils/get-timezones.ts
+++ src/utils/get-timezones.ts
@@ -1,4 +1,6 @@
 export const getTimezones = async (): Promise<string[]> => {
   const response = await import("timezones-list");
-  return response.default.map((tz) => tz.tzCode);
+  return response.default
+    .map((tz) => tz.tzCode)
+    .sort((prev = "", next = "") => prev.localeCompare(next));
 };
